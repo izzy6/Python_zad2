240d2b6e8a65a9daaaac6c16e5d2e49a68d02c67
Anna Pietrzak <195680@edu.p.lodz.pl>
2018-11-12 18:04:12 +0100

    przygotowanie danych, imputacja

--- zad3/main.py
+++ zad3/main.py
@@ -7,33 +7,70 @@ def readDataset():
     'horsepower', 'peak-rpm', 'city-mpg', 'highway-mpg', 'price'])
     #replace ? missing values with python NaN - values with a NaN value are ignored from operations like sum, count
     data = data.replace('?', numpy.NaN)
+
+    #trzeba zamienic niektore kolumny w ktorych brakuje danych na typ numeryczny zeby nie powodowaly problemow w dalszej czesci programu
+    data["normalized-losses"] = pd.to_numeric(data["normalized-losses"])
+    data["horsepower"] = pd.to_numeric(data["horsepower"])
+    data["peak-rpm"] = pd.to_numeric(data["peak-rpm"])
+    data["price"] = pd.to_numeric(data["price"])
     return data
+
+def printMissingValues(dataset):
+    print("\nBrakujące dane według kolumn")
+    print("\nrow_num        missing_val")
+    print(dataset.isnull().sum())
 
 def countMissingValues(dataset):
     
-    print("Brakujące dane według kolumn")
-    print("row_num        missing_val")
-    print(dataset.isnull().sum())
+    printMissingValues(dataset)
     #print(dataset.head(20))
     nan_count = 0
     for index, row in dataset.iterrows():
         if (row.isna().any()):
             nan_count += 1
-    print("Liczba rekordów z co najmniej jedną brakującą wartością: %d" % nan_count)
+    print("\nLiczba rekordów z co najmniej jedną brakującą wartością: %d" % nan_count)
     percentage = nan_count*100/dataset.shape[0]
     print("Procent rekordów z co najmniej jedną brakującą wartością: %d %%" % percentage)
 
-def getDatasetWithoutMissingValues(dataset):
-    print("Kasowanie rekordów zawierających braki...")
-    print("Rozmiar bazy przed kasowaniem:")
+def deleteRowsWithMissingValues(dataset):
+    print("\nKasowanie rekordów zawierających braki...")
+    print("\nRozmiar bazy przed kasowaniem:")
     print(dataset.shape)
-    dataset = dataset.dropna(axis=0, how='any')
+    dataset.dropna(axis=0, how='any', inplace=True)
     #print(dataset.head(20))
-    print("Rozmiar bazy po skasowaniu:")
+    print("\nRozmiar bazy po skasowaniu:")
     print(dataset.shape)
+    #print(dataset.isnull().sum())
     return dataset
+
+def fillMissingValuesWithMeans(dataset, datasetWithDeletedRows):
+    print("\nZastepowanie brakujacych wartosci srednimi z kolumn...")
+    dataset.fillna(dataset.mean(), inplace=True)
+    printMissingValues(dataset)
+    print(dataset.head(20))
+    #print(dataset.mean())
+    return dataset
+
+def regressionFunction(dataset):
+    #TODO: 3. Wyznaczyć krzywą regresji dla danych bez braków.
+    print("To be continued...")
+
+def compareDatasetsBeforeAndAfterImputation(datasetBefore, datasetAfter):
+    #TODO: 5. Porównać charakterystykę zbiorów przed i po imputacji (średnia, odchylenie standardowe, kwartyle).
+    print("To be continued...")
+
+def regressionFunctionAfterImputation():
+    #TODO: 6. Wyznaczyć krzywą regresji dla danych po imputacji. Porównać jak zmieniły się parametry krzywej.
+    print("To be continued...")
+    
 dataset = readDataset()
+
 countMissingValues(dataset)
-datasetWithoutMissingVal = getDatasetWithoutMissingValues(dataset)
+
+datasetWithDeletedRows = deleteRowsWithMissingValues(dataset)
+
+datasetWithFilledMeanValues = fillMissingValuesWithMeans(dataset, datasetWithDeletedRows)
+
+
 
 
