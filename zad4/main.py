f3dbefc0796e23d13ff286150d3c744f1d96de4a
Anna Pietrzak <195680@edu.p.lodz.pl>
2018-12-03 13:31:10 +0100

    klasyfikacja i rysowanie wykresu

--- zad4/main.py
+++ zad4/main.py
@@ -1,8 +1,11 @@
 import pandas as pd
-from sklearn import svm
-from sklearn.model_selection import train_test_split  
+import numpy as np
+from sklearn.linear_model import SGDClassifier
+from sklearn.neural_network import MLPClassifier
+from sklearn import model_selection  
 from sklearn.svm import SVC 
-from sklearn.metrics import classification_report, confusion_matrix  
+from sklearn.metrics import classification_report, confusion_matrix, accuracy_score  
+import matplotlib.pyplot as plt
  
 
 def readDataset():
@@ -17,16 +20,37 @@ def readDataset():
     #print(data)
     return data
 
+def drawPlot(scores_test, scores_train):
+    plt.plot(scores_train, color='green', alpha=0.8, label='Trening')
+    plt.plot(scores_test, color='magenta', alpha=0.8, label='Test')
+    plt.title("Dokładność w zależności od l. iteracji", fontsize=14)
+    plt.xlabel('Iteracje')
+    plt.legend(loc='upper left')
+    plt.show()
+
+#na podstawie https://stackoverflow.com/questions/46912557/is-it-possible-to-get-test-scores-for-each-iteration-of-mlpclassifier
+def classification(data):
+    #dzielimy zbior danych na dane treningowe i testowe w proporcji 8:2
+    x = data.drop('class', axis=1)
+    y = data['class']
+    x_train, x_test, y_train, y_test = model_selection.train_test_split(x, y, test_size = 0.20) 
+
+    clf = SGDClassifier()
+
+    iterations = 50
+    scores_train = []
+    scores_test = []
+
+    for i in range(iterations):
+        #clf.partial_fit(x_train, y_train, classes=np.unique(y_train))
+        clf.fit(x_train, y_train)
+        scores_train.append(clf.score(x_train, y_train))
+        scores_test.append(clf.score(x_test, y_test))
+
+    drawPlot(scores_test, scores_train)
+
 
 data = readDataset()
-x = data.drop('class', axis=1)
-y = data['class']
-x_train, x_test, y_train, y_test = train_test_split(x, y, test_size = 0.20) 
+classification(data)
 
-svmclassifier = SVC(kernel='linear', max_iter=1)  
-svmclassifier.fit(x_train, y_train)
 
-y_pred = svmclassifier.predict(x_test)  
-
-print(confusion_matrix(y_test,y_pred))  
-print(classification_report(y_test,y_pred))  
