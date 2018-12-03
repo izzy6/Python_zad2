4927b0ca384bafad64e65e8d5e1c51613a078276
Anna Pietrzak <195680@edu.p.lodz.pl>
2018-12-03 14:40:27 +0100

    punkt 3 i 4 na ocene 3

--- zad4/main.py
+++ zad4/main.py
@@ -1,7 +1,8 @@
 import pandas as pd
 import numpy as np
 from sklearn.linear_model import SGDClassifier
-from sklearn.neural_network import MLPClassifier
+from sklearn.preprocessing import StandardScaler
+from sklearn.decomposition import PCA
 from sklearn import model_selection  
 from sklearn.svm import SVC 
 from sklearn.metrics import classification_report, confusion_matrix, accuracy_score  
@@ -33,24 +34,65 @@ def classification(data):
     #dzielimy zbior danych na dane treningowe i testowe w proporcji 8:2
     x = data.drop('class', axis=1)
     y = data['class']
-    x_train, x_test, y_train, y_test = model_selection.train_test_split(x, y, test_size = 0.20) 
-
-    clf = SGDClassifier()
+    x_train, x_test, y_train, y_test = model_selection.train_test_split(x, y, test_size = 0.20)
 
     iterations = 50
     scores_train = []
     scores_test = []
 
     for i in range(iterations):
+        clf = SGDClassifier(n_iter=i)
         #clf.partial_fit(x_train, y_train, classes=np.unique(y_train))
         clf.fit(x_train, y_train)
         scores_train.append(clf.score(x_train, y_train))
         scores_test.append(clf.score(x_test, y_test))
 
+
     drawPlot(scores_test, scores_train)
 
+# na podstawie: https://towardsdatascience.com/pca-using-python-scikit-learn-e653f8989e60
+def pcaReduction(data):
+    x = data.drop('class', axis=1)
+    y = data['class']
+
+    # Standardizing the features - bez skalowania algorytm PCA może nie działać
+    x = StandardScaler().fit_transform(x)
+
+    #redukacja z 4 wymiarów(atrybutów) do 2
+    pca = PCA(n_components=2)
+    principalComponents = pca.fit_transform(x)
+    dataAfterReduction = pd.DataFrame(data = principalComponents, columns = ['komponent 1', 'komponent 2'])
+    #print(dataAfterReduction)
+    reducedDataWithClasses = pd.concat([dataAfterReduction, y], axis = 1)
+    #print(dataWithClasses)
+    
+    #ta czesc nie jest potrzebna - wyswietla dane na wykresie po redukcji
+    fig = plt.figure(figsize = (8,8))
+    ax = fig.add_subplot(1,1,1) 
+    ax.set_xlabel('Principal Component 1', fontsize = 15)
+    ax.set_ylabel('Principal Component 2', fontsize = 15)
+    ax.set_title('2 component PCA', fontsize = 20)
+    targets = ['Iris-setosa', 'Iris-versicolor', 'Iris-virginica']
+    colors = ['r', 'g', 'b']
+    for target, color in zip(targets,colors):
+        indicesToKeep = reducedDataWithClasses['class'] == target
+        ax.scatter(reducedDataWithClasses.loc[indicesToKeep, 'komponent 1']
+                , reducedDataWithClasses.loc[indicesToKeep, 'komponent 2']
+                , c = color
+                , s = 50)
+    ax.legend(targets)
+    ax.grid()
+    plt.show()
+    ###############################################
+
+
+    return reducedDataWithClasses
 
 data = readDataset()
 classification(data)
 
+reducedData = pcaReduction(data)
 
+classification(reducedData)
+
+print('Po redukcji klasyfikacja jest jakby dokładniejsza(?)')
