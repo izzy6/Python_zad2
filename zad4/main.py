4df8a588dfacb924555041edbae6ee16791b5fe8
Izabela Zbieć <izbiec@stud.ics.p.lodz.pl>
2018-12-04 13:12:42 +0100

    Ostateczna wersja zad 4

--- zad4/main.py
+++ zad4/main.py
@@ -3,7 +3,7 @@ import numpy as np
 from sklearn.linear_model import SGDClassifier
 from sklearn.preprocessing import StandardScaler
 from sklearn.decomposition import PCA
-from sklearn import model_selection  
+from sklearn import model_selection
 from sklearn.svm import SVC 
 from sklearn.metrics import classification_report, confusion_matrix, accuracy_score  
 import matplotlib.pyplot as plt
@@ -12,25 +12,22 @@ import scipy.stats.contingency as cn
 
 def readDataset():
     data = pd.read_csv('database/iris.data', names = ['swidth', 'slength', 'pwidth', 'plength', 'class'])
-    #print(data)
 
     #change type of data in car2 dataset
     data["swidth"] = pd.to_numeric(data["swidth"])
     data["slength"] = pd.to_numeric(data["slength"])
     data["pwidth"] = pd.to_numeric(data["pwidth"])
     data["plength"] = pd.to_numeric(data["plength"])
-    #print(data)
     return data
 
 def drawPlot(scores_test, scores_train):
-    plt.plot(scores_train, color='green', alpha=0.8, label='Trening')
-    plt.plot(scores_test, color='magenta', alpha=0.8, label='Test')
+    plt.plot(scores_train, color='green', alpha=0.8, label='Przed redukcją')
+    plt.plot(scores_test, color='magenta', alpha=0.8, label='Po redukcji')
     plt.title("Dokładność w zależności od l. iteracji", fontsize=14)
     plt.xlabel('Iteracje')
     plt.legend(loc='upper left')
     plt.show()
 
-#na podstawie https://stackoverflow.com/questions/46912557/is-it-possible-to-get-test-scores-for-each-iteration-of-mlpclassifier
 def classification(data):
     #dzielimy zbior danych na dane treningowe i testowe w proporcji 8:2
     x = data.drop('class', axis=1)
@@ -48,10 +45,27 @@ def classification(data):
         scores_train.append(clf.score(x_train, y_train))
         scores_test.append(clf.score(x_test, y_test))
 
+    #drawPlot(scores_test, scores_train)
+    return scores_test
 
-    drawPlot(scores_test, scores_train)
+def classificationAfterChanges(data, clasifiedData):
+    #dzielimy zbior danych na dane treningowe i testowe w proporcji 8:2
+    x = data.drop('class', axis=1)
+    y = data['class']
+    x_train, x_test, y_train, y_test = model_selection.train_test_split(x, y, test_size = 0.20)
 
-# na podstawie: https://towardsdatascience.com/pca-using-python-scikit-learn-e653f8989e60
+    iterations = 50
+    scores_train = []
+    scores_test = []
+
+    for i in range(iterations):
+        clf = SGDClassifier(n_iter=i)
+        #clf.partial_fit(x_train, y_train, classes=np.unique(y_train))
+        clf.fit(x_train, y_train)
+        scores_train.append(clf.score(x_train, y_train))
+        scores_test.append(clf.score(x_test, y_test))
+    drawPlot(scores_test, clasifiedData)
+
 def pcaReduction(data):
     x = data.drop('class', axis=1)
     y = data['class']
@@ -67,7 +81,7 @@ def pcaReduction(data):
     reducedDataWithClasses = pd.concat([dataAfterReduction, y], axis = 1)
     #print(dataWithClasses)
     
-    #ta czesc nie jest potrzebna - wyswietla dane na wykresie po redukcji
+    #wyswietla dane na wykresie po redukcji
     fig = plt.figure(figsize = (8,8))
     ax = fig.add_subplot(1,1,1) 
     ax.set_xlabel('Principal Component 1', fontsize = 15)
@@ -84,7 +98,6 @@ def pcaReduction(data):
     ax.legend(targets)
     ax.grid()
     plt.show()
-    ###############################################
     return reducedDataWithClasses
 
 def test_independence(data, colX, colY):
@@ -93,13 +106,16 @@ def test_independence(data, colX, colY):
     observed = pd.crosstab(Y,X)
     chi2, p, dof, expected = cn.chi2_contingency(observed.values)
     return p
+
+import warnings
+warnings.simplefilter("ignore")
 #3
 data = readDataset()
 dataClear = data.copy()
-classification(data)
+trainData = classification(data)
 reducedData = pcaReduction(data)
-classification(reducedData)
-print('Po redukcji klasyfikacja jest jakby dokładniejsza')
+classificationAfterChanges(reducedData,  trainData)
+#print('Po redukcji klasyfikacja jest jakby dokładniejsza')
 
 #4
 # Wariancja
@@ -108,7 +124,7 @@ dataVar = np.power(dataStd, 2)
 print(dataVar)
 print("Najmniejszą wariancję mają cechy: slength i plenght")
 dataMinVar = pd.DataFrame(dataClear, columns=['slength', 'plength', 'class'])
-classification(dataMinVar)
+classificationAfterChanges(dataMinVar, trainData)
 # Test niezależności chi**2
 dataChi = []
 labels = list(dataClear)
@@ -119,7 +135,7 @@ for i in range(4):
 minChi = sorted(dataChi, key=lambda x: x[2])[0]
 print('Najmniejszą zależność({}) na podstawie testu chi**2 mają cechy: {} i {}'.format(minChi[2], minChi[0], minChi[1]))
 dataMinChi = pd.DataFrame(dataClear, columns=[ minChi[0],  minChi[1], 'class'])
-classification(dataMinChi)
+classificationAfterChanges(dataMinChi,  trainData)
 
 
 
